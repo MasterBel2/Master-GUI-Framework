fce8b102d60b6eb1269559a0ef28168ddc0af644
MasterBel2 <untakenprefix@gmail.com>
2023-05-29 17:47:15 +1000

    Add FlowUI Extension
    Currently only adds helper for drawing window background

---
+++ gui_master_framework_flowui_extensions.lua
@@ -0,0 +1,28 @@
+function widget:GetInfo()
+    return {
+        name = "MasterFramework FlowUI Extensions",
+        description = "Integrates FlowUI drawing to assist in matching default styles",
+        layer = -math.huge -- Run Initialise after MasterFramework file has loaded, but before any widget uses it
+    }
+end
+
+local requiredFrameworkVersion = 14
+
+function widget:Initialize()
+    local MasterFramework = WG.MasterFramework[requiredFrameworkVersion]
+    if MasterFramework and WG.FlowUI.Draw then
+        local FlowUIExtensions = {}
+        MasterFramework.FlowUIExtensions = FlowUIExtensions
+
+        function FlowUIExtensions:Element()
+            local element = {}
+
+            function element:Draw(rect, x, y, width, height)
+                WG.FlowUI.Draw.Element(x, y, x + width, y + height)
+            end
+
+            return element
+        end
+
+    end
+end
