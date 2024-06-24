a0b37db4697b2f088fe8532c7fe52ea549645040
MasterBel2 <untakenprefix@gmail.com>
2024-06-24 21:40:19 +1000

    Document `Box`

--- MasterFramework 42/Framework/Box.lua
+++ MasterFramework 42/Framework/Box.lua
@@ -1,3 +1,10 @@
+--[[
+    Box is a component that does not modify layout other than to allow you to swith its child. 
+    Since most compoments will not allow you to modify their child, provide `Box` as a component's 
+    child to allow you to modify the view hierarchy.
+
+    Call `box:SetChild(newChild)` to update `Box`'s child.
+]]
 function framework:Box(child)
 	local box = Component(true, false)
 
