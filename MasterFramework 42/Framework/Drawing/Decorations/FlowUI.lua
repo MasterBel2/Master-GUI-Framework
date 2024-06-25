dbef83ffbec3eb0fd8af8498f02df544beab268d
MasterBel2 <untakenprefix@gmail.com>
2024-06-25 11:32:39 +1000

    Remove redundant code from `FlowUIExtensions:Element()` decoration

--- MasterFramework 42/Framework/Drawing/Decorations/FlowUI.lua
+++ MasterFramework 42/Framework/Drawing/Decorations/FlowUI.lua
@@ -5,10 +5,6 @@ framework.FlowUIExtensions = FlowUIExtensions
 function FlowUIExtensions:Element()
     local element = {}
 
-    function element:NeedsRedrawForDrawer()
-        return false
-    end
-
     function element:Draw(rect, x, y, width, height)
         WG.FlowUI.Draw.Element(x, y, x + width, y + height)
     end
