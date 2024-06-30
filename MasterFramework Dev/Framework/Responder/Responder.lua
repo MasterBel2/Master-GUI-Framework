988d953c02243737ef8a0bfa9fd8b5e32573403c
MasterBel2 <untakenprefix@gmail.com>
2024-06-30 13:20:46 +1000

    Remove obsolete debug code from `Responder`

--- MasterFramework Dev/Framework/Responder/Responder.lua
+++ MasterFramework Dev/Framework/Responder/Responder.lua
@@ -8,8 +8,7 @@ Internal.activeResponders = {}
 local responderID = 0
 
 function framework:Responder(rect, event, action)
-	local responder = { action = action, responders = {}, id = responderID, _event = event }
-	responderID = responderID + 1
+	local responder = { action = action, responders = {}, _event = event }
 
 	local width, height
 	local cachedX, cachedY
@@ -18,31 +17,12 @@ function framework:Responder(rect, event, action)
 	function responder:CachedPosition() return cachedX, cachedY end
 	function responder:Geometry() return cachedX, cachedY, width, height end
 
-	local laidOut
 	function responder:Layout(...)
-		laidOut = true
 		width, height = rect:Layout(...)
 		return width, height
 	end
 
 	function responder:Position(x, y)
-		if not laidOut then
-			Log("We havent laid out yet!")
-			Log(self._debugTypeIdentifier)
-			Log(self._debugUniqueIdentifier)
-			if self._debug_mouseOverResponder then
-				local path = self._debugTypeIdentifier
-				local x = self._debug_mouseOverResponder.parent
-				for i = 1, 1000 do
-					if not x then break end
-					path = (x._debugTypeIdentifier or "Unknown") .. "/" .. path
-					x = x.parent
-				end
-				Log(path)
-			end
-		end
-		if not rect then Log("We dont have a rect!") end
-
 		-- Parent keeps track of the order of responders, and use that to decide who gets the interactions first
 		local previousActiveResponder = Internal.activeResponders[event]
 		if previousActiveResponder then
