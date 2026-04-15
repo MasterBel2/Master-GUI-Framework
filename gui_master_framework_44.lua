d3ff567d91ab2891ac4ca1344704ec4e554ed393
MasterBel2 <untakenprefix@gmail.com>
2026-04-15 12:49:05 +1000

    CV44-rev2

--- gui_master_framework_44.lua
+++ gui_master_framework_44.lua
@@ -13,7 +13,7 @@ function widget:GetInfo()
 		desc = "A GUI framework for the SpringRTS Engine",
 		author = "MasterBel2",
 		date = "October 2020",
-		version = "44-rev1"
+		version = "44-rev2",
 		license = "GNU GPL, v2 or later",
 		layer = -2,
 		enabled = true,
